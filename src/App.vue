9f7f4a2f690bbb24e8eb8ef756e54f644b82bb15
Dimas Kurniawan <ichbindimas@gmail.com>
2024-02-03 23:10:32 +0700

    sample test

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,26 @@
     <div class="content-wrapper">
       <div class="content">
         <div class="button-tab">
-          <Button label="Input" @click="tabHandler('input')" severity="secondary" :outlined="tab.input ? false : true" />
-          <Button label="Table" @click="tabHandler('table')" severity="secondary" :outlined="tab.table ? false : true" />
-          <Button label="Summary" @click="tabHandler('summary')" severity="secondary" :outlined="tab.summary ? false : true" />
+          <Button data-test="click-input" label="Input" @click="tabHandler('input')" severity="secondary" :outlined="tab.input ? false : true" />
+          <Button data-test="click-table" label="Table" @click="tabHandler('table')" severity="secondary" :outlined="tab.table ? false : true" />
+          <Button data-test="click-summary" label="Summary" @click="tabHandler('summary')" severity="secondary" :outlined="tab.summary ? false : true" />
         </div>
         <Divider />
         <!--INPUT-->
         <div class="content-input" v-if="tab.input">
           <div class="user-input">
-            <label for="username">Generate Random Number</label>
-            <InputText type="number" id="username" v-model="inputNumber" style="width: 300px" />
-            <small id="username-help">Enter a number between 1 - 10000</small>
+            <div class="user-input_input">
+              <label for="username">Generate Random Number</label>
+              <InputText data-test="input" type="number" id="username" v-model="inputNumber" style="width: 300px" />
+              <small id="username-help">Enter a number between 1 - 10000</small>
+            </div>
+            <div class="user-input_button">
+              <Button label="Generate" @click="generateRandomNumbers" :disabled="!isEnableGenerateButton" />
+            </div>
           </div>
-          <div>
-            <Button label="Generate" @click="generateRandomNumbers" :disabled="!isEnableGenerateButton" />
+          <div class="quote">
+            <h5>Quote of the day</h5>
+            <h1>`{{ quote }}`</h1>
           </div>
         </div>
         <!--TABLE-->
@@ -25,6 +31,7 @@
           <div v-if="randomNumbers.length < 1" class="empty-table">
             <h1>PLEASE GENERATE NEW NUMBERS</h1>
           </div>
+          <h3 style="color: white; margin-bottom: 16px">You have generated {{ randomNumbers.length }}</h3>
           <table v-for="(item, idx) of splitArrayData">
             <thead>
               <tr>
@@ -45,7 +52,11 @@
             <Button label="Get Summary" :disabled="randomNumbers.length < 1" @click="findSmallestNumber" />
           </div>
           <div class="summary" v-if="summary">
-            <h1 style="color: white">{{ summary }}</h1>
+            <h1 style="color: white" data-test="summry-value">Sumary is : {{ summary }}</h1>
+            <div class="quote">
+              <h5>Quote of the day</h5>
+              <h1>`{{ quote }}`</h1>
+            </div>
           </div>
         </div>
       </div>
@@ -54,105 +65,137 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref, watch, reactive } from "vue";
-const inputNumber = ref<number>();
-const randomNumbers = ref<number[]>([]);
-const splitArrayData = ref<number[][]>([]);
-const summary = ref<number>(0);
-const isEnableGenerateButton = ref(false);
-const tab = reactive({
-  input: true,
-  table: false,
-  summary: false,
-});
-const items = ref([
-  {
-    label: "Dimas Kurniawan",
-    icon: "pi pi-home",
-  },
-]);
+<script lang="ts">
+import { ref, watch, reactive, onMounted, defineComponent } from "vue";
+export default defineComponent({
+  setup() {
+    const inputNumber = ref<number>(0);
+    const randomNumbers = ref<number[]>([]);
+    const splitArrayData = ref<number[][]>([]);
+    const summary = ref<number>(0);
+    const isEnableGenerateButton = ref(false);
+    const quote = ref("");
+    const tab = reactive({
+      input: true,
+      table: false,
+      summary: false,
+    });
+    const items = ref([
+      {
+        label: "Dimas Kurniawan",
+        icon: "pi pi-home",
+      },
+    ]);
 
-watch(
-  inputNumber,
-  (val = 0) => {
-    if (val > 0 && val < 10001) {
-      isEnableGenerateButton.value = true;
-    } else {
-      isEnableGenerateButton.value = false;
+    watch(
+      inputNumber,
+      (val = 0) => {
+        if (val > 0 && val < 10001) {
+          isEnableGenerateButton.value = true;
+        } else {
+          isEnableGenerateButton.value = false;
+        }
+      },
+      { immediate: true }
+    );
+
+    watch(
+      () => tab.input,
+      () => {
+        if (inputNumber.value && inputNumber.value > 0 && inputNumber.value < 10001) {
+          isEnableGenerateButton.value = true;
+        } else {
+          isEnableGenerateButton.value = false;
+        }
+      }
+    );
+
+    function generateRandomNumbers() {
+      randomNumbers.value = [];
+      summary.value = 0;
+      if (inputNumber.value !== undefined) {
+        for (let i = 0; i < inputNumber.value; i++) {
+          const randomNumber = Math.floor(Math.random() * (1000000 - -1000000 + 1)) + -1000000;
+          randomNumbers.value.push(randomNumber);
+        }
+        isEnableGenerateButton.value = false;
+        splitArray(randomNumbers.value, 10);
+      }
     }
-  },
-  { immediate: true }
-);
 
-watch(
-  () => tab.input,
-  () => {
-    if (inputNumber.value && inputNumber.value > 0 && inputNumber.value < 10001) {
-      isEnableGenerateButton.value = true;
-    } else {
-      isEnableGenerateButton.value = false;
-    }
-  }
-);
+    function splitArray(array: any, size: number) {
+      const temp = [];
 
-function generateRandomNumbers() {
-  randomNumbers.value = [];
-  summary.value = 0;
-  // toast.add({ severity: "warn", summary: "Invalid Input", detail: "Enter a number between 1 - 10000", life: 3000 });
-  if (inputNumber.value !== undefined) {
-    for (let i = 0; i < inputNumber.value; i++) {
-      const randomNumber = Math.floor(Math.random() * (1000000 - -1000000 + 1)) + -1000000;
-      randomNumbers.value.push(randomNumber);
-    }
-    isEnableGenerateButton.value = false;
-    splitArray(randomNumbers.value, 10);
-  }
-}
+      for (let i = 0; i < array.length; i += size) {
+        temp.push(array.slice(i, i + size));
+      }
 
-function splitArray(array: any, chunkSize: number) {
-  const chunks = [];
-
-  for (let i = 0; i < array.length; i += chunkSize) {
-    chunks.push(array.slice(i, i + chunkSize));
-  }
-
-  splitArrayData.value = chunks;
-  tab.input = false;
-  tab.table = true;
-}
-
-function findSmallestNumber() {
-  const sortArray = randomNumbers.value.sort((a, b) => a - b).filter((el) => el > 0);
-  summary.value = sortArray[0] - 1;
-}
-
-function tabHandler(activeTab: string) {
-  switch (activeTab) {
-    case "input":
-      tab.input = true;
-      tab.table = false;
-      tab.summary = false;
-      break;
-    case "table":
+      splitArrayData.value = temp;
       tab.input = false;
       tab.table = true;
-      tab.summary = false;
-      break;
-    case "summary":
-      tab.input = false;
-      tab.table = false;
-      tab.summary = true;
-      break;
-    default:
-      break;
-  }
-}
+    }
+
+    function findSmallestNumber() {
+      const sortArray = randomNumbers.value.sort((a, b) => a - b).filter((el) => el > 0);
+      summary.value = sortArray[0] - 1;
+    }
+
+    function tabHandler(activeTab: string) {
+      switch (activeTab) {
+        case "input":
+          tab.input = true;
+          tab.table = false;
+          tab.summary = false;
+          break;
+        case "table":
+          tab.input = false;
+          tab.table = true;
+          tab.summary = false;
+          break;
+        case "summary":
+          tab.input = false;
+          tab.table = false;
+          tab.summary = true;
+          break;
+        default:
+          break;
+      }
+    }
+
+    // https://github.com/lukePeavey/quotable //
+    async function generateRandomQuote() {
+      try {
+        const response = await fetch("https://api.quotable.io/random");
+        const res = await response.json();
+        quote.value = res.content;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    onMounted(() => {
+      generateRandomQuote();
+    });
+    return {
+      inputNumber,
+      quote,
+      splitArrayData,
+      isEnableGenerateButton,
+      tab,
+      items,
+      randomNumbers,
+      summary,
+      generateRandomNumbers,
+      findSmallestNumber,
+      tabHandler,
+    };
+  },
+});
 </script>
 
 <style scoped lang="scss">
 #main-wrapper {
-  background-image: url("../public/1329016.png");
+  background-image: url("/1329016.png");
   background-size: cover;
   display: flex;
   flex-direction: column;
@@ -181,13 +224,37 @@ function tabHandler(activeTab: string) {
 
       .content-input {
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         align-items: center;
-        gap: 8px;
+        gap: 80px;
         .user-input {
           display: flex;
-          flex-direction: column;
+          flex-direction: row;
           gap: 6px;
+          align-items: center;
+          width: 100%;
+          color: white;
+          &_input {
+            display: flex;
+            flex-direction: column;
+            gap: 6px;
+          }
+        }
+      }
+      .quote {
+        background: rgba(255, 255, 255, 0.53);
+        border-radius: 16px;
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(9.1px);
+        -webkit-backdrop-filter: blur(9.1px);
+        border: 1px solid rgba(255, 255, 255, 0.61);
+        text-align: center;
+        // color: white;
+        font-weight: bolder;
+        width: 60%;
+        padding: 16px;
+        h1 {
+          font-style: italic;
         }
       }
 
@@ -198,6 +265,11 @@ function tabHandler(activeTab: string) {
         flex-direction: row;
         justify-content: center;
         align-items: center;
+      }
+      .summary {
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
       }
 
       .content-table,
---
+++ src/test/App.spec.ts
@@ -0,0 +1,22 @@
+import { mount } from "@vue/test-utils";
+import AppVue from "../App.vue";
+
+describe("AppVue Component", () => {
+  const wrapper = mount(AppVue);
+  it("should update inputNumber and call generateRandomNumbers", async () => {
+    const vm = wrapper.vm;
+
+    vm.inputNumber = 10;
+
+    await vm.generateRandomNumbers();
+
+    expect(vm.inputNumber).toBe(10);
+    expect(vm.randomNumbers.length).toBe(10);
+  });
+
+  it("Click Summary Tab", async () => {
+    const summaryTab = wrapper.find('[data-test="click-summary"]');
+    summaryTab.trigger("click");
+    expect(wrapper.vm.tab.summary).toBe(true);
+  });
+});
